3f584150358665576e8a8e131f4ecd2595d242b0
Joel Pavlicek <18955848@student.westernsydney.edu.au>
2023-05-25 11:22:55 +1000

    Added new import option to pipeline that takes the JSON data a request model usually provides and performs the template transform and other operations of that model before inserting it into the database.

--- auto_labeling_pipeline/pipeline.py
+++ auto_labeling_pipeline/pipeline.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from auto_labeling_pipeline.labels import Labels
 from auto_labeling_pipeline.mappings import MappingTemplate
 from auto_labeling_pipeline.models import RequestModel
@@ -10,5 +11,14 @@ def pipeline(text: str,
              post_processing: BasePostProcessor) -> Labels:
     response = request_model.send(text)
     labels = mapping_template.render(response)
+    labels = post_processing.transform(labels)
+    return labels
+
+
+def pipeline_import(text: str,
+             annotations: Dict[Any, Any],
+             mapping_template: MappingTemplate,
+             post_processing: BasePostProcessor) -> Labels:
+    labels = mapping_template.render(annotations)
     labels = post_processing.transform(labels)
     return labels
